4d951fbf97ae625b8701d9577455464732a0bc15
Sean Farley <sean@farley.io>
2018-12-19 14:23:41 -0600

    style: fix linting warnings about docstrings

--- lsp-python-ms.el
+++ lsp-python-ms.el
@@ -32,25 +32,31 @@
 (require 'cl)
 
 (defvar lsp-python-ms-dir nil
-  "Path to langeuage server directory containing Microsoft.Python.LanguageServer.dll")
+  "Path to language server directory.
+
+This is the directory containing Microsoft.Python.LanguageServer.dll.")
 
 (defvar lsp-python-ms-dotnet nil
-  "Full path to dotnet executable. You only need to set this if dotnet is not on your path.")
+  "Full path to dotnet executable.
+
+You only need to set this if dotnet is not on your path.")
 
 (defvar lsp-python-ms-executable
   (cond
    ((executable-find "Microsoft.Python.LanguageServer"))
    ((executable-find "Microsoft.Python.LanguageServer.exe"))
    (t nil))
-  "Path to Microsoft.Python.LanguageServer.exe")
+  "Path to Microsoft.Python.LanguageServer.exe.")
 
 ;; it's crucial that we send the correct Python version to MS PYLS,
 ;; else it returns no docs in many cases furthermore, we send the
 ;; current Python's (can be virtualenv) sys.path as searchPaths
 
 (defun lsp-python-ms--get-python-ver-and-syspath (workspace-root)
-  "Return list with pyver-string and json-encoded list of python
-search paths."
+  "Return list with pyver-string and list of python search paths.
+
+The WORKSPACE-ROOT will be prepended to the list of python search
+paths and then the entire list will be json-encoded."
   (let ((python (executable-find python-shell-interpreter))
         (init "from __future__ import print_function; import sys; import json;")
         (ver "print(\"%s.%s\" % (sys.version_info[0], sys.version_info[1]));")
@@ -63,6 +69,9 @@ search paths."
 ;; https://github.com/Microsoft/vscode-python/blob/master/src/client/activation/languageServer/languageServer.ts#L219
 ;; (it still took quite a while to get right, but here we are!)
 (defun lsp-python-ms--extra-init-params (&optional workspace)
+  "Return extra initialization params.
+
+Optionally add the WORKSPACE to the python search list."
   (let ((workspace-root (if workspace (lsp--workspace-root workspace) (pwd))))
     (destructuring-bind (pyver pysyspath)
       (lsp-python-ms--get-python-ver-and-syspath workspace-root)
@@ -107,14 +116,15 @@ search paths."
       (setq rx (concat rx "\\|\r")))
     (replace-regexp-in-string rx " " str)))
 
+(defun lsp-python-ms--language-server-started-callback (workspace _params)
+  "Handle the python/languageServerStarted message.
 
-(defun lsp-python-ms--language-server-started-callback (workspace params)
-  "Handle the python/languageServerStarted message"
+WORKSPACE is just used for logging and _PARAMS is unused."
   (lsp-workspace-status "::Started" workspace)
   (message "Python language server started"))
 
 (defun lsp-python-ms--client-initialized (client)
-  "Callback to register and configure the client after it's initialized"
+  "Callback to register and configure the CLIENT after it's initialized."
   (lsp-client-on-notification client "python/languageServerStarted" 'lsp-python-ms--language-server-started-callback))
 
 ;; this gets called when we do lsp-describe-thing-at-point
@@ -132,7 +142,7 @@ search paths."
             :filter-return #'lsp-python-ms--filter-nbsp)
 
 (defun lsp-python-ms--command-string ()
-  "Return the command that starts the server."
+  "Return the command to start the server."
   (if lsp-python-ms-executable
       lsp-python-ms-executable
     (list (lsp-python-ms--find-dotnet)
