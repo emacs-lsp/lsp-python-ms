2b6bd9ebb67d490c9e4758b73ec1ad141bd773d5
Vincent Zhang <seagle0128@gmail.com>
2019-11-01 03:48:12 +0800

    Bump version.

--- lsp-python-ms.el
+++ lsp-python-ms.el
@@ -2,7 +2,7 @@
 
 ;; Author: Charl Botha
 ;; Maintainer: Andrew Christianson, Vincent Zhang
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Package-Requires: ((cl-lib "0.6.1") (lsp-mode "6.0") (python "0.26.1") (json "1.4") (emacs "24.4"))
 ;; Homepage: https://github.com/andrew-christianson/lsp-python-ms
 ;; Keywords: languages tools
